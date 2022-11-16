77fbb4be32a38d1f9b3c50e719bf5938d1001bd7
E_yair_lopez <eylopez@mail.csuchico.edu>
2022-11-16 11:53:25 -0800

    Modified files

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -19,7 +19,14 @@ TicTacToeBoard::TicTacToeBoard()
 **/
 Piece TicTacToeBoard::toggleTurn()
 {
-  return Invalid;
+  // Checks if X or O has the turn
+  if(turn == X){
+    turn = O;
+  }else if(turn == O){
+    turn = X;
+  }
+  // returns the piece
+  return turn;
 }
 
 /**
@@ -33,6 +40,13 @@ Piece TicTacToeBoard::toggleTurn()
 **/ 
 Piece TicTacToeBoard::placePiece(int row, int column)
 {
+  Piece play = getPiece(row, column);
+  if(play == Blank){
+    toggleTurn();
+    return turn;
+  }else{
+    return play;
+  }
   return Invalid;
 }
 
@@ -42,6 +56,10 @@ Piece TicTacToeBoard::placePiece(int row, int column)
 **/
 Piece TicTacToeBoard::getPiece(int row, int column)
 {
+  if(row<0 || row >= BOARDSIZE || column<0 || column >= BOARDSIZE){
+    return Invalid;
+  }
+  return board[row][column];
   return Invalid;
 }
 
@@ -51,5 +69,29 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 **/
 Piece TicTacToeBoard::getWinner()
 {
-  return Invalid;
+  //rows
+  for(int i=0; i < BOARDSIZE; i++)
+  {
+    if(board[i][0] == board[i][1] && board[i][1] == board[i][2])
+      return board[i][0];
+  }
+  //columns
+  for(int i=0; i<BOARDSIZE; i++)
+  {
+    if(board[0][i] == board[1][i] && board[1][i] == board[2][i])
+      return board[0][i];
+  }
+  //diagonals
+  if(board[0][0] == board[1][1] && board[1][1] == board[2][2])
+    return board[0][0];
+  if(board[0][2] == board[1][1] && board[1][1] == board[2][0])
+    return board[0][2];
+
+  //board is full
+  for(int i=0; i<BOARDSIZE; i++)
+    for(int j=0; j<BOARDSIZE; j++)
+      if(board[i][j] == Blank)
+        return Invalid;
+
+  return Blank;
 }
--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -14,5 +14,22 @@ class TicTacToeBoardTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
+TEST(TicTacToeBoardTest, workingPiecesX){
+	TicTacToeBoard myTest;
+	int row[1] = {X};
+	ASSERT_EQ(row[0], X);
+}
+TEST(TicTacToeBoardTest, workingPiecesO){
+	TicTacToeBoard myTest;
+	int row[2] = {O};
+	// ASCII value
+	ASSERT_EQ(row[2], 32);
+}
+TEST(TicTacToeBoardTest, winnerX){
+	TicTacToeBoard myTest;
+	Piece myPiece = myPiece.getPiece();
+	// ASCII value
+	ASSERT_EQ(myPiece, 32);
+}
 
 
